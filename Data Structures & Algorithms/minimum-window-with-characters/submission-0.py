d790e9395a5f4f80a5ef90e0eaf4f4e1ec157e6b
hritikboss <138761371+hritikboss@users.noreply.github.com>
2026-06-20 23:36:25 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        ans = ""
+
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+
+                sub = s[i:j+1]
+
+                ok = True
+
+                for ch in t:
+                    if sub.count(ch) < t.count(ch):
+                        ok = False
+                        break
+
+                if ok:
+                    if ans == "" or len(sub) < len(ans):
+                        ans = sub
+        return ans
+
+
